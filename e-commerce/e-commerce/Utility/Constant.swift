7683925b797b34f66e0880355a681452bdce14e1
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-06 01:03:43 +0700

    add resource

---
+++ e-commerce/e-commerce/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 6/3/2564 BE.
+//
+
+import Foundation
---
+++ e-commerce/e-commerce/View/Home/FooterView.swift
@@ -0,0 +1,20 @@
+//
+//  FooterView.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 6/3/2564 BE.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
