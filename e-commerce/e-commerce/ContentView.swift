8b8520cb065c7a6f9ec5a92a24f3fca0309cf6a5
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-06 00:06:21 +0700

    initial project e-commerce

---
+++ e-commerce/e-commerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 6/3/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ e-commerce/e-commerce/e_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_commerceApp.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 6/3/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct e_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
