df709495a4a8d657d9928e5214e76bdd38079c22
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-06 01:04:52 +0700

    add constant

--- e-commerce/e-commerce/Utility/Constant.swift
+++ e-commerce/e-commerce/Utility/Constant.swift
@@ -5,4 +5,20 @@
 //  Created by Phonthep Aungkanukulwit on 6/3/2564 BE.
 //
 
-import Foundation
+import SwiftUI
+
+// DATA
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
