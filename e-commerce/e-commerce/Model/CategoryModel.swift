96ebecce3cd4180c1c61da090e5ee7aa4ac60c65
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-07 17:13:15 +0700

    CategoryModel

---
+++ e-commerce/e-commerce/Model/CategoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryModel.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 7/3/2564 BE.
+//
+
+import Foundation
+
+struct Catetory: Codable, Identifiable {
+  let id: Int
+  let name: String
+  let image: String
+}
