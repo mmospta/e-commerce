b865d9c499fd59cd79e05f2ff6721218ef481e6e
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-07 18:54:16 +0700

    Add Brand model

---
+++ e-commerce/e-commerce/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 7/3/2564 BE.
+//
+
+import Foundation
+
+struct Brand {
+  let id: Int
+  let image: String
+}
