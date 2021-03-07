4911abe63dc95b95cb9284535f2e317f502d6a8c
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-07 16:38:05 +0700

    model

---
+++ e-commerce/e-commerce/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 7/3/2564 BE.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+  let id: Int
+  let image: String
+}
