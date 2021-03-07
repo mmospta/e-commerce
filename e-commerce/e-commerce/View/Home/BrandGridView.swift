8b15bbe940bddacfc2f022ca6e76022dfdc889cc
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-07 19:16:47 +0700

    Add BrandGridView

---
+++ e-commerce/e-commerce/View/Home/BrandGridView.swift
@@ -0,0 +1,36 @@
+//
+//  BrandGridView.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 7/3/2564 BE.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+  // MARK: - PROPERTIES
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    ScrollView(.horizontal, showsIndicators: false, content: {
+      LazyHGrid(rows: gridLayout, spacing: columnSpacing, content: {
+        ForEach(brands) { brand in
+          BrandItemView(brand: brand)
+        }
+      }) //: GRID
+      .frame(height: 200)
+      .padding(15)
+    }) //: SCROLL
+  }
+}
+
+// MARK: - PREVIEW
+
+struct BrandGridView_Previews: PreviewProvider {
+  static var previews: some View {
+    BrandGridView()
+      .previewLayout(.sizeThatFits)
+      .background(colorBackground)
+  }
+}
