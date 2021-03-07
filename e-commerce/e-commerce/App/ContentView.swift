5d6c2fb09a0cbcb5b034ec95a58313c410c438b2
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-07 17:09:06 +0700

    Slider View

--- e-commerce/e-commerce/App/ContentView.swift
+++ e-commerce/e-commerce/App/ContentView.swift
@@ -17,17 +17,21 @@ struct ContentView: View {
     ZStack {
       VStack(spacing: 0) {
         NavigationBarView()
-          //          .padding()
           .padding(.horizontal, 15)
           .padding(.bottom)
           .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
           .background(Color.white)
           .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
         
-        Spacer()
-        
-        FooterView()
-          .padding(.horizontal)
+        ScrollView(.vertical, showsIndicators: false, content: {
+          VStack(spacing: 0) {
+            FeaturedTabView()
+              .padding(.vertical, 20)
+            
+            FooterView()
+              .padding(.horizontal)
+          }
+        }) //: SCROLL
       } //: VSTACK
       .background(colorBackground.ignoresSafeArea(.all, edges: .all))
     } //: ZSTACK
---
+++ e-commerce/e-commerce/View/Home/FeaturedTabView.swift
@@ -0,0 +1,30 @@
+//
+//  FeaturedTabView.swift
+//  e-commerce
+//
+//  Created by Phonthep Aungkanukulwit on 7/3/2564 BE.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+  var body: some View {
+    TabView {
+      
+      ForEach(players) { player in
+        FeaturedItemView(player: player)
+          .padding(.top, 10)
+          .padding(.horizontal, 15)
+      }
+    } //: TAB
+    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+  }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+  static var previews: some View {
+    FeaturedTabView()
+      .previewDevice("iPhone 12 Pro")
+      .background(Color.gray)
+  }
+}
